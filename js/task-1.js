f28f8edeb780f7325f5a7ab6c4925ba8bf336fc7
olenka <elena.kaminnik@gmail.com>
2024-07-13 23:07:25 +0300

    add task-1.js file

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,10 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {}
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalPrice = quantity * pricePerDroid;
+  return totalPrice <= customerCredits
+    ? `You ordered $(quantity) droids worth $(totalPrice) credits!`
+    : `Insufficient funds!`;
+}
+
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
