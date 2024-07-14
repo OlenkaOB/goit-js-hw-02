452483a1955c9dfdf023613e13d4eac7e6983c69
olenka <elena.kaminnik@gmail.com>
2024-07-14 14:38:54 +0300

    add task-1.js file

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
   return totalPrice <= customerCredits
-    ? `You ordered $(quantity) droids worth $(totalPrice) credits!`
+    ? `You ordered ${quantity} droids worth ${totalPrice} credits!`
     : `Insufficient funds!`;
 }
 
