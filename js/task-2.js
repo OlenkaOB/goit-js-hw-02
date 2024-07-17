ae757ed35d40e6d079181b4807bfc36044640c04
olenka <elena.kaminnik@gmail.com>
2024-07-17 14:42:54 +0300

    add task file

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,11 @@
 function formatMessage(message, maxLength) {
-  return message.length <= maxLength ? message : maxLength + `...`;
+  if (message.length <= maxLength) {
+    const expression = message.slice();
+    return expression;
+  } else {
+    const answer = `${message.slice(0, maxLength)}...`;
+    return answer;
+  }
 }
 console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
 console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
